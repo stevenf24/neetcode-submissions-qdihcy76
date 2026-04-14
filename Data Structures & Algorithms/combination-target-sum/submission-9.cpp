8454a9ddd9507ddda5b5374b698467a4f06364a7
stevenf24 <73857264+stevenf24@users.noreply.github.com>
2026-04-14 15:38:35 -0400

    Add: combination-target-sum - submission-9

---
+++ Data Structures & Algorithms/combination-target-sum/submission-9.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    // Create an array at global level
+    // that will store the potential combinations:
+    vector<vector<int>> res;
+
+    
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        // Sort the array:
+        sort(nums.begin(), nums.end());
+        
+        // Send the 
+        dfs(0, {}, 0, nums, target);
+        return res;
+
+    }
+        
+
+    void dfs(int i, vector<int> current, int total, vector<int>& nums,
+                            int target) {
+        if(total == target) {
+            res.push_back(current);
+            return;
+        }
+
+        for(int j = i; j < nums.size(); j++) {
+            if(total + nums[j] > target) {
+                return;
+            }
+
+            current.push_back(nums[j]);
+
+            dfs(j, current, total + nums[j], nums, target);
+
+            current.pop_back();
+        }
+        
+    }
+
+    
+};
