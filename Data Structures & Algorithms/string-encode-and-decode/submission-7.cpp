fd5779a08030f1c98e09134973c7508b704039ba
stevenf24 <73857264+stevenf24@users.noreply.github.com>
2026-08-03 16:36:13 -0400

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string res = "";
+
+        for(const string& s : strs) {
+            res.append(to_string(s.size()));
+            res.push_back('#');
+            res.append(s);
+        }
+
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        int i = 0;
+        vector<string> res;
+
+        while(i < s.size()) {
+            int j = i;
+            while(s[j] != '#') {
+                j += 1;
+            }
+
+            int length = stoi(s.substr(i, j - i));
+            i = j + 1;
+            j = i + length;
+            res.push_back(s.substr(i, length));
+            i = j;
+        }
+
+        return res;
+    }
+};
