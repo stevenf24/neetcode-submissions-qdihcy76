986d68086497cfbdc899b99d2c1479af1e6f57c2
stevenf24 <73857264+stevenf24@users.noreply.github.com>
2026-09-01 10:32:33 -0400

    Add: duplicate-integer - submission-64

---
+++ Data Structures & Algorithms/duplicate-integer/submission-64.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        if(nums.size() == 0)
+            return false;
+
+        unordered_set<int> res;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(res.count(nums[i]))
+                return true;
+            
+            res.insert(nums[i]);
+        }
+
+        return false;
+    }
+};
