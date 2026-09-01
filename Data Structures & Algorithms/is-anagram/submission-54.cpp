8f2b3a4a441cece61b4d2b24766cb4c7e6e77005
stevenf24 <73857264+stevenf24@users.noreply.github.com>
2026-09-01 14:23:11 -0400

    Add: is-anagram - submission-54

---
+++ Data Structures & Algorithms/is-anagram/submission-54.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
+
+        std::unordered_map<char, int> countS;
+        std::unordered_map<char, int> countT;
+
+        for(int i = 0; i < s.length(); i++) {
+            countS[s[i]]++;
+            countT[t[i]]++;
+        }
+
+        return countS == countT;
+    }
+};
